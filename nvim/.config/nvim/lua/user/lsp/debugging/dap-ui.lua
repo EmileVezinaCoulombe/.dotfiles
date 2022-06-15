172b36cdc997c438135fdc3f7d266db38ebd112e
EmileVezinaCoulombe <emilevezinacoulombe@icloud.com>
2022-06-14 23:05:07 -0400

    feat : auto open debugging ui

--- nvim/.config/nvim/lua/user/lsp/debugging/dap-ui.lua
+++ nvim/.config/nvim/lua/user/lsp/debugging/dap-ui.lua
@@ -1,6 +1,20 @@
-local status_ok, dap_ui = pcall(require, "dapui")
+local status_ok, dap = pcall(require, "dap")
 if not status_ok then
     return
+end
+local dapui_status_ok, dap_ui = pcall(require, "dapui")
+if not dapui_status_ok then
+    return
+end
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dap_ui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dap_ui.close()
 end
 
 dap_ui.setup({
