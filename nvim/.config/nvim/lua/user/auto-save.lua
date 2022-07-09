7df02919d3a57193146ab20717c55e00e57553fb
EmileVezinaCoulombe <emilevezinacoulombe@icloud.com>
2022-07-09 16:31:02 -0400

    feat: Add auto-save

--- nvim/.config/nvim/lua/user/auto-save.lua
+++ nvim/.config/nvim/lua/user/auto-save.lua
@@ -1,7 +1,14 @@
-local status_ok, auto_save = pcall( require, "autosave")
+local status_ok, auto_save = pcall(require, "autosave")
 
 if not status_ok then
     return
 end
 
-auto_save.setup({execution_message = "",})
+auto_save.setup({ execution_message = "",
+    events = { "InsertLeave", "TextChanged" },
+    conditions = {
+        exists = true,
+        filename_is_not = { "plugins.lua" },
+        modifiable = true
+    },
+})
