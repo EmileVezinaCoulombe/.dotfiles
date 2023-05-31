2a3beab15058a1ad54c98258e69804470514deeb
EmileVezinaCoulombeMecart <evezina-coulombe@mecart.com>
2023-05-30 22:19:25 -0400

    feat: add diffview

--- AppData/Local/nvim/lua/plugins/diffview.lua
+++ AppData/Local/nvim/lua/plugins/diffview.lua
@@ -1,8 +1,32 @@
+local function getMainBrach()
+  local root_dir = require("lazyvim.util").get_root()
+  if root_dir:find("Nomis") then
+    return "Developper"
+  end
+  return "main"
+end
 return {
   {
     "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-    config = true,
-    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+    opts = {
+      default_args = {
+        DiffviewOpen = { "--imply-local" },
+        DiffviewFileHistory = { "--imply-local" },
+      },
+    },
+    keys = {
+      {
+        "<leader>gb",
+        "<cmd>DiffviewOpen origin/" .. getMainBrach() .. "...HEAD --imply-local<cr>",
+        desc = "DiffView Branche changes",
+      },
+      {
+        "<leader>gB",
+        "<cmd>:DiffviewFileHistory --range=origin/" .. getMainBrach() .. "...HEAD --right-only --no-merges<cr>",
+        desc = "DiffView Branche commits",
+      },
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView Current changes" },
+      { "<leader>g%", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffView File History" },
+    },
   },
 }
